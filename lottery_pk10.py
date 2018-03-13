a06694ef5a0e80a23642984f10aafc97027ac7a8
Kun-Min Chiu <chchiuleroy@gmail.com>
2018-03-13 21:14:27 +0800

    Update lottery_pk10.py

--- lottery_pk10.py
+++ lottery_pk10.py
@@ -5,7 +5,7 @@ Created on Wed Feb 28 23:13:10 2018
 @author: roy
 """
 
-import numpy as np, pandas as pd, matplotlib.pyplot as plt
+import numpy as np, pandas as pd, matplotlib.pyplot as plt, random
 
 a = 1; b = 11; r = 10
 runs = 1000000; people = 30
@@ -15,10 +15,9 @@ pay = np.array([0, 0, 0, 44.145, 44.145, 22.070, 22.070, 14.695, 14.695, 11.025,
 bet = np.array([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1])
 
 def test(a, b, r, pay, bet):
-    out = np.array([np.random.choice(range(a,b), size = r, replace = False)])[:,0:2].sum()
+    out = np.array([random.sample(range(a,b), r)])[:,0:2].sum()
     reward = pay[out]*bet[out]
     return reward
 
-process = np.array([[test(a, b, r, pay, bet) for j1 in range(runs)]for j2 in range(people)]).mean(axis = 1)/bet.sum()
-
+process = np.array([test(a, b, r, pay, bet) for j1 in range(runs)for j2 in range(people)]).reshape(runs, people).mean(axis = 0)/bet.sum()
 plt.boxplot(process); plt.show()
